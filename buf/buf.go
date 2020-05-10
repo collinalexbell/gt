53f999b2a520d1859a7533cc7564c5ccb66fd317
slightlycyborg <collinalexbell@gmail.com>
2020-05-10 00:35:17 +0000

    buf: ViewLines

--- buf/buf.go
+++ buf/buf.go
@@ -1,13 +1,20 @@
 package buf
 
+import "strings"
+
 type Buffer struct {
-	s string
+	s []string
 }
 
 func FromString(s string) *Buffer {
-	return &Buffer{s: s}
+	slice := strings.Split(s, "\n")
+	return &Buffer{s: slice}
 }
 
-func (b *Buffer) View() string {
+func (b *Buffer) View() []string {
 	return b.s
+}
+
+func (b *Buffer) ViewLines(start int64, stop int64) []string {
+	return b.View()[start:stop]
 }
--- buf/test/buf_test.go
+++ buf/test/buf_test.go
@@ -6,10 +6,22 @@ import (
 )
 
 func TestCreateAndView(t *testing.T) {
+	str := "this\nis\na buffer\n"
+	b := buf.FromString(str)
+	actual := b.View()
+	expected := []string{"this", "is", "a buffer"}
+	for i := range expected {
+		if expected[i] != actual[i] {
+			t.Fail()
+		}
+	}
+}
+
+func TestViewWithIndices(t *testing.T) {
 	expected := "this\nis\na buffer\n"
 	b := buf.FromString(expected)
-	actual := b.View()
-	if expected != actual {
-		t.Fail()
+	actual := b.ViewLines(0, 2)
+	if len(actual) != 2 || "this" != actual[0] || "is" != actual[1] {
+		t.Errorf("actual: %s", actual)
 	}
 }
